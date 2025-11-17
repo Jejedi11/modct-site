bda188d13c1e9c531091ba0ce24303f1e505a903
James Michelson <jejedi110@gmail.com>
2025-11-16 22:29:14 -0700

    nav menu

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector("#menu")
+const menuNav = document.querySelector("nav")
+
+menuButton.addEventListener("click", function(){
+    menuNav.classList.toggle("open")
+    menuButton.classList.toggle("open")
+})
