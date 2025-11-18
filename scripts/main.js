2941e6f44dd30c31bc40a4b8bb43c603ec082bf1
James Michelson <jejedi110@gmail.com>
2025-11-17 22:05:58 -0700

    Added the events system

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,12 @@ menuButton.addEventListener("click", function(){
     menuNav.classList.toggle("open")
     menuButton.classList.toggle("open")
 })
+
+function MakeEvent(name, people, date, details){
+    const event = document.createElement("div");
+    event.innerHTML = "<h3>" + name + "</h3><h4>" + date + "</h4><p>" + details + "</p><h4>" + people + "</h4>";
+    document.querySelector('.events').appendChild(event);
+}
+
+MakeEvent("Jousting", "Jimbus", "05/11/2022", "Join us for the world famous jousting!")
+MakeEvent("Burgon Feast", "Jimbus and Sadam", "Dec 12", "We will be feasting burgons, please come baoss!")
