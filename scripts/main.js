a34c205f700e15f0d11ebbda2cc782c98a918114
James <jejedi110@pm.me>
2025-12-13 23:58:54 -0700

    Total Overhaul

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,28 @@
-const menuButton = document.querySelector("#menu")
-const menuNav = document.querySelector("nav")
-
-menuButton.addEventListener("click", function(){
-    menuNav.classList.toggle("open")
-    menuButton.classList.toggle("open")
-})
-
-function MakeEvent(name, people, date, details){
+class Event {
+  constructor(name, people, date, details) {
+    this.name = name;
+    this.people = people;
+    this.date = date;
+    this.details = details;
+  }
+  MakeEvent() {
     const event = document.createElement("div");
-    event.innerHTML = "<h3>" + name + "</h3><h4>" + date + "</h4><p>" + details + "</p><h4>" + people + "</h4>";
+    event.innerHTML = "<h3>" + this.name + "</h3><h4>" + this.date + "</h4><p>" + this.details + "</p><h4>" + this.people + "</h4>";
     document.querySelector('.events').appendChild(event);
+  }
 }
 
-MakeEvent("Jousting", "Jimbus", "05/11/2022", "Join us for the world famous jousting!")
-MakeEvent("Burgon Feast", "Jimbus and Sadam", "Dec 12", "We will be feasting burgons, please come baoss!")
+let day = new Date(2023, 6, 1, 12, 12, 12);
+let annie = new Event("Annie Performance", "Adam", new Date(2025, 11, 11, 7, 0, 0).toString(), "Performing on the 11th-12th and 18th-20th");
+let christmasEve = new Event("Christmas Eve", "Everyone", new Date(2025, 11, 24).toString(), "Merry Christmas!");
+let christmas = new Event("Christmas Day", "Everyone", new Date(2025, 11, 25).toString(), "Merry Christmas!");
+let newYears = new Event("New Years Day", "Everyone", new Date(2026, 1, 1).toString(), "Happy New Years!");
+let events = [annie, christmas, christmasEve, newYears];
+
+events.sort(function(a, b) { return new Date(b.date) - new Date(a.date); });
+
+events.forEach((item) => {
+  item.MakeEvent();
+});
+
+
